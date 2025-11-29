a94feaeabf355aa21192b7e2c3219a2db9006138
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-29 18:45:08 -0300

    "feat: Create ErrorMessageDTO and enhance exception handling"

---
+++ src/main/java/br/com/ruangomes/api_cursos/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.ruangomes.api_cursos.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
