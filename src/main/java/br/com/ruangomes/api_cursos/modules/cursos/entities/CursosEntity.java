3a6472094d19fed29b80f17aad429a30a82d0dfb
Ruan.Dev <ruanlopes21@gmail.com>
2025-11-29 13:18:17 -0300

    "feat: Create CursosEntity class with Lombok annotations"

--- src/main/java/br/com/ruangomes/api_cursos/modules/cursos/entities/CursosEntity.java
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/entities/CursosEntity.java
@@ -1,8 +1,25 @@
 package br.com.ruangomes.api_cursos.modules.cursos.entities;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CursosEntity {
     
+    private UUID id;
+    private String name;
+    private String category;
+    private Boolean active;
+    private Date created_at;
+    private LocalDateTime updated_at;
 }
