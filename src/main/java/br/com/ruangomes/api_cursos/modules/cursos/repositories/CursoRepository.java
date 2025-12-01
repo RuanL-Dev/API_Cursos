2ca731ca072ea00f505e9753262f7903171aea85
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-01 11:54:10 -0300

    "feat: Implements repository for CustosEntity"

---
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/repositories/CursoRepository.java
@@ -0,0 +1,12 @@
+package br.com.ruangomes.api_cursos.modules.cursos.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.ruangomes.api_cursos.modules.cursos.entities.CursosEntity;
+
+public interface CursoRepository extends JpaRepository<CursosEntity, UUID> {
+    Optional<CursosEntity> findByNameOrCategory(String name, String category);
+}
