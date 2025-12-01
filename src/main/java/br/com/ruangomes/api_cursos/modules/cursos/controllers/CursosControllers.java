d832e889a69fdd94ed61708fea62ae2c56cfa3d5
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-01 11:54:54 -0300

    "feat: Implement controller for creating courses"

--- src/main/java/br/com/ruangomes/api_cursos/modules/cursos/controllers/CursosControllers.java
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/controllers/CursosControllers.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ruangomes.api_cursos.modules.cursos.entities.CursosEntity;
+import br.com.ruangomes.api_cursos.modules.cursos.useCases.CreateCursoUseCase;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,10 +17,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/cursos")
 public class CursosControllers {
 
+    @Autowired
+    private CreateCursoUseCase createCursoUseCase;
+
     @PostMapping("/")
-    public void create( @Valid @RequestBody CursosEntity cursosEntity) {
-        System.out.println("Cursos");
-        System.out.println(cursosEntity.getName());
+    public ResponseEntity<Object> create( @Valid @RequestBody CursosEntity cursosEntity) {
+        try {
+            var result = this.createCursoUseCase.execute(cursosEntity);
+            return ResponseEntity.ok().body(result);
+            
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
         
         
     }
