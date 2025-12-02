3b8eb254cce9dc9aedf3bcf27ff2d73455bdbb5c
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-01 22:12:24 -0300

    "feat: Creates autentication use case with JWT token generation and HMAC256 algorithm with an Response DTO"

---
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/dto/AuthCursoResponseDTO.java
@@ -0,0 +1,15 @@
+package br.com.ruangomes.api_cursos.modules.cursos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthCursoResponseDTO {
+    private String access_token;
+    private Long expires_in;
+}
