bed127afa10d930c98f8d8aa775378874fead865
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-03 09:44:33 -0300

    "feat: Create route authenticated and links with subject coming from token JWT"

--- src/main/java/br/com/ruangomes/api_cursos/modules/cursos/repositories/CursoRepository.java
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/repositories/CursoRepository.java
@@ -1,5 +1,6 @@
 package br.com.ruangomes.api_cursos.modules.cursos.repositories;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -7,7 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.ruangomes.api_cursos.modules.cursos.entities.CursosEntity;
 
+
 public interface CursoRepository extends JpaRepository<CursosEntity, UUID> {
     Optional<CursosEntity> findByNameOrCategory(String name, String category);
     Optional<CursosEntity> findByName(String name);
+    List<CursosEntity> findByProfessor_NomeProfessor(String nomeProfessor);
+    List<CursosEntity> findByProfessor_Id(UUID professorId);
 }
