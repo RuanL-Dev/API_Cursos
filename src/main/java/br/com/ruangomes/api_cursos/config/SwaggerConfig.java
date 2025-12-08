8879f6d47a09fa0665a1d65dcf79c403704edfe7
Ruan.Dev <ruanlopes21@gmail.com>
2025-12-07 21:38:28 -0300

    "fix: Adjusting error typing in SwaggerConfig.java"

--- src/main/java/br/com/ruangomes/api_cursos/config/SwaggerConfig.java
+++ src/main/java/br/com/ruangomes/api_cursos/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
 
         return new OpenAPI()
-        .info(new Info().title("Gestão de Cursos API").description("API para criaçaõ de cursos").version("1.0.0"))
+        .info(new Info().title("Gestão de Cursos API").description("API para criação de cursos").version("1.0.0"))
         .schemaRequirement("jwt_auth", createSecurityScheme());
     }
 
