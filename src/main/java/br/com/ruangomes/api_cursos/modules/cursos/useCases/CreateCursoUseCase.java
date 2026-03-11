266bd583239c448616ee2599c956ad02939391dc
Ruan Gomes <ruanlopes21@gmail.com>
2026-03-11 13:56:24 -0300

    Implement full Unit Testing for CreateCursoUseCase

--- src/main/java/br/com/ruangomes/api_cursos/modules/cursos/useCases/CreateCursoUseCase.java
+++ src/main/java/br/com/ruangomes/api_cursos/modules/cursos/useCases/CreateCursoUseCase.java
@@ -1,11 +1,8 @@
 package br.com.ruangomes.api_cursos.modules.cursos.useCases;
 
 import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
 import br.com.ruangomes.api_cursos.exceptions.CursoFoundException;
 import br.com.ruangomes.api_cursos.modules.cursos.dto.CreateCursoRequestDTO;
 import br.com.ruangomes.api_cursos.modules.cursos.dto.ProfileCursoResponseDTO;
@@ -18,18 +15,16 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class CreateCursoUseCase {
-    
 
     private final CursoRepository cursoRepository;
 
     private final ProfessorRepository professorRepository;
 
-
     public ProfileCursoResponseDTO execute(CreateCursoRequestDTO createCursoRequestDTO) {
         this.cursoRepository.findByNameIgnoreCase(createCursoRequestDTO.getName())
-        .ifPresent((user) -> {
-            throw new CursoFoundException();
-        });
+                .ifPresent((user) -> {
+                    throw new CursoFoundException();
+                });
 
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         var subject = authentication.getName();
@@ -37,31 +32,30 @@ public class CreateCursoUseCase {
         UUID professorId;
         try {
             professorId = UUID.fromString(subject);
-        } catch (IllegalArgumentException  e) {
+        } catch (IllegalArgumentException e) {
             throw new RuntimeException("Token inválido: subject não é um UUID");
         }
-        
 
         ProfessorEntity professor = professorRepository.findById(professorId)
-        .orElseThrow(() -> {
-            throw new RuntimeException("Professor not found");
-        });
+                .orElseThrow(() -> {
+                    throw new RuntimeException("Professor not found");
+                });
 
         var cursoBuilder = CursosEntity.builder()
-        .name(createCursoRequestDTO.getName())
-        .category(createCursoRequestDTO.getCategory())
-        .professor(professor)
-        .build();
+                .name(createCursoRequestDTO.getName())
+                .category(createCursoRequestDTO.getCategory())
+                .professor(professor)
+                .build();
 
         var saved = this.cursoRepository.save(cursoBuilder);
 
         var cursoDTO = ProfileCursoResponseDTO.builder()
-        .name(saved.getName())
-        .category(saved.getCategory())
-        .professor(saved.getProfessor().getNomeProfessor())
-        .active(saved.getActive())
-        .id(saved.getId())
-        .build();
+                .name(saved.getName())
+                .category(saved.getCategory())
+                .professor(saved.getProfessor().getNomeProfessor())
+                .active(saved.getActive())
+                .id(saved.getId())
+                .build();
 
         return cursoDTO;
     }
